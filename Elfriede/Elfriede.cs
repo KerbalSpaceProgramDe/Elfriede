bbbc7f6c309fe08ec039024e915d3945f0b7d256
Dorian Stoll <dorian@tmsp.ga>
2016-10-21 17:30:21 +0200

    C# Projekt

---
+++ Elfriede/Elfriede.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Elfriede
+{
+    /// <summary>
+    /// PartModule that applies a random upward force to the part, if the player is within a certain range
+    /// </summary>
+    public class Elfriede : PartModule
+    {
+        [KSPField(isPersistant = false)] public Single range;
+        [KSPField(isPersistant = false)] public Int32 minForce;
+        [KSPField(isPersistant = false)] public Int32 maxForce;
+        [KSPField(isPersistant = false)] public Int32 count;
+        [KSPField(isPersistant = true)] public Boolean jumped;
+
+        void FixedUpdate()
+        {
+            // Distance is smaller
+            Single dist = Vector3.Distance(FlightGlobals.ActiveVessel.transform.position, transform.position);
+            if (dist < range && !jumped)
+            {
+                System.Random r = new System.Random();
+                for (Int32 i = 0; i < count; i++)
+                    part.rb.AddExplosionForce(r.Next(minForce, maxForce), transform.position, 5, 0);
+                jumped = true;
+            }
+            else if (dist >= range && jumped)
+            {
+                jumped = false;
+            }
+        }
+    }
+}
---
+++ Elfriede/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Elfriede")]
+[assembly: AssemblyDescription("Dancing stuff")]
+[assembly: AssemblyProduct("Elfriede")]
+[assembly: AssemblyCopyright("Copyright © kerbal.de 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2b883cd6-d0ba-4b31-95d1-7b5454cbfe5b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
